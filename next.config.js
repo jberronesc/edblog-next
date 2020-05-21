99545afb093f6fef055e4b35f8364e40ba1b0471
Jefferson Berrones <berronesjefferson@gmail.com>
2020-05-21 14:51:03 -0500

    Configuracion de varaibles de entorno y dependecias

---
+++ next.config.js
@@ -0,0 +1,26 @@
+// archivo de configuracion
+
+const { PHASE_DEVELOPMENT_SERVER } = require( 'next/constants' )
+
+module.exports  = phase => {
+  
+  const isDevlopment = phase === PHASE_DEVELOPMENT_SERVER
+
+  const env = {
+    SITE_NAME : 'EDBlog',
+    API_BLOG : ( () => {
+      if ( isDevlopment ) {
+        return "https://jsonplaceholder.typicode.com/"
+      } else {
+        return "https://api.edblog.com"
+      }
+    } )(), //funcion sobre funcion, y que la primera funcion me va a retornar un callback
+  }
+
+  return {
+    env,
+  }
+  // env : {
+  //   SITE_NAME : 'EDBlog'
+  // }
+}
